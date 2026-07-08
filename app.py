78270ad6aa375021e29c325a19aa7efe78e8a53d
nguyenvanbao201 <hunghackgamek9@gmail.com>
2026-07-08 15:38:04 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import json
+import os
+import random
+import string
+from datetime import datetime
+
+app = Flask(__name__)
+
+KEY_FILE = "free_keys.json"
+
+
+def load_keys():
+    if not os.path.exists(KEY_FILE):
+        return {}
+
+    with open(KEY_FILE, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except:
+            return {}
+
+
+def save_keys(data):
+    with open(KEY_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+
+@app.route("/")
+def home():
+    return "VB TOOL FREE SERVER OK"
